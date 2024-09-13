c752910cc659c659b5212ba41b225f751cc33d3c
Marcelo Afonso <dev.marceloafonso@gmail.com>
2024-09-12 21:49:25 -0300

    feat: diagrama, projeto java web

---
+++ back-end/PI2-PROJETO/src/main/java/controller/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public Connection recuperarConexao() {
+        try {
+            return DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/mercado_facil?user=root&password=root");
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
